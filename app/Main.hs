23b6c0cb834995f6d0176183ff96d88ae3844233
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-04-23 14:16:51 +0200

    Make the example a little bit more fun.

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,6 @@ import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever, void)
 import Data.ByteString qualified as BS
 import Data.IORef (IORef, newIORef, readIORef, writeIORef)
-import Debug.Trace (traceShowId)
 import GHC.Generics (Generic)
 import Network.Run.TCP (runTCPClient, runTCPServer)
 import Network.Socket.ByteString (recv, sendAll)
@@ -19,8 +18,8 @@ updateRef f r = readIORef r >>= writeIORef r . f
 readRef :: IORef a -> IO a
 readRef = readIORef
 
-updateCounter :: IORef Env -> IO ()
-updateCounter = updateRef $ \env -> env{counter = counter env + 1}
+updateCounter :: Int -> IORef Env -> IO ()
+updateCounter i = updateRef $ \env -> env{counter = counter env + i}
 getCounter :: IORef Env -> IO Int
 getCounter = (counter <$>) . readIORef
 
@@ -35,8 +34,8 @@ serve rawE = do
   void . runTCPServer (Just "localhost") "8000" $ \s -> do
     msg <- debin <$> recv s 1024
     case msg of
-      UpdateCounter _ -> do
-        updateCounter e
+      UpdateCounter i -> do
+        updateCounter i e
       GetCounter -> do
         i <- getCounter e
         sendAll s $ BS.pack [fromIntegral i]
@@ -46,7 +45,7 @@ main = do
   void . forkIO . serve $ Env 0
   forever $ do
     runTCPClient "localhost" "8000" $ \s -> do
-      sendAll s (bin (UpdateCounter 2))
+      sendAll s (bin (UpdateCounter 3))
     a <- runTCPClient "localhost" "8000" $ \s -> do
       sendAll s (bin GetCounter)
       recv s 1024
